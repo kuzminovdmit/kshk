d76726f29dcc602cffb883702e72c8df86cd50f4
Dmitry Kuzminov <kuzminovdmit@gmail.com>
2022-06-22 03:07:56 +0300

    Create Parser class

--- youtube_parser.py
+++ youtube_parser.py
@@ -14,24 +14,29 @@ logging.basicConfig(
 )
 
 
-def auth() -> googleapiclient.discovery.Resource:
-    logger = logging.getLogger('youtube_parser.auth')
-    store = Storage('credentials.json')
-    credentials = store.get()
-        
-    if not credentials or credentials.invalid:
-        logger.info('credentials.json doesn\'t exist, trying to authenticate in browser')
-        flow = client.flow_from_clientsecrets(
-            'client_secret_file.json', 
-            [
-                'https://www.googleapis.com/auth/youtube.readonly',
-                'https://www.googleapis.com/auth/youtube.force-ssl'
-            ]
+class Parser:
+    def __init__(self):
+        self.client = self.auth()
+
+    @staticmethod
+    def auth() -> googleapiclient.discovery.Resource:
+        logger = logging.getLogger('youtube_parser.auth')
+        store = Storage('credentials.json')
+        credentials = store.get()
+
+        if not credentials or credentials.invalid:
+            logger.info('credentials.json doesn\'t exist, trying to authenticate in browser')
+            flow = client.flow_from_clientsecrets(
+                'client_secret_file.json',
+                [
+                    'https://www.googleapis.com/auth/youtube.readonly',
+                    'https://www.googleapis.com/auth/youtube.force-ssl'
+                ]
+            )
+            credentials = tools.run_flow(flow, store)
+
+        logger.info(f'Successfully retrieved credentials')
+
+        return googleapiclient.discovery.build(
+            'youtube', 'v3', credentials=credentials
         )
-        credentials = tools.run_flow(flow, store)
-    
-    logger.info(f'Successfully retrieved credentials')
-    
-    return googleapiclient.discovery.build(
-        'youtube', 'v3', credentials=credentials
-    )
